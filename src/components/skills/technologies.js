68c260f505c81700244749239fe1d8563632b930
ciraganenicole <ciraganenicole@gmail.com>
2022-11-16 12:10:06 +0200

    Update skills

--- src/components/skills/technologies.js
+++ src/components/skills/technologies.js
@@ -7,10 +7,11 @@ const Technologies = () => {
             <li className="m-2 border-2 p-1 rounded-lg">Html5</li>
             <li className="m-2 border-2 p-1 rounded-lg">CSS3</li>
             <li className="m-2 border-2 p-1 rounded-lg">TailwindCSS</li>
-            <li className="m-2 border-2 p-1 rounded-lg">Chakra UI</li>
-            <li className="m-2 border-2 p-1 rounded-lg">Styled Components</li>
             <li className="m-2 border-2 p-1 rounded-lg">JavaScript</li>
             <li className="m-2 border-2 p-1 rounded-lg">React</li>
+            <li className="m-2 border-2 p-1 rounded-lg">Ruby</li>
+            <li className="m-2 border-2 p-1 rounded-lg">Ruby on rails</li>
+            <li className="m-2 border-2 p-1 rounded-lg">PostgreSql</li>
         </ul>
         </>
     )
